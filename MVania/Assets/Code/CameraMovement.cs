eca9a867508daadc06f525e495f49e4e7685be46
Johan-1 <nilssonjohan10@gmail.com>
2017-12-21 20:26:32 +0100

    add camera script and room loader

---
+++ MVania/Assets/Code/CameraMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine;
+
+
+public class CameraMovement : MonoBehaviour 
+{
+
+    [SerializeField] Vector2 _XClampMinMax;
+    [SerializeField] Vector2 _YClampMinMax;
+
+    Transform _target;
+    Vector3 _vel;
+
+	void Start () 			
+	{
+        _target = FindObjectOfType<PlayerMovement>().transform;
+
+        Vector3 desiredPosition = GetDesiredPosition();
+        transform.position = new Vector3( desiredPosition.x, desiredPosition.y ,transform.position.z);
+
+    }
+
+    void LateUpdate()
+    {
+
+        Vector3 desiredPosition = GetDesiredPosition();
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(desiredPosition.x,desiredPosition.y,transform.position.z), ref _vel, 0.25f);
+    }
+
+
+    Vector3 GetDesiredPosition()
+    {
+        Vector3 desiredPosition = _target.transform.position + new Vector3(0, 0, 0);
+        desiredPosition.x = Mathf.Clamp(desiredPosition.x, _XClampMinMax.x, _XClampMinMax.y);
+        desiredPosition.y = Mathf.Clamp(desiredPosition.y, _YClampMinMax.x, _YClampMinMax.y);
+
+        return desiredPosition;
+
+    }
+
+
+}
---
+++ MVania/Assets/Code/RoomLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class RoomLoader : MonoBehaviour 
+{
+
+
+    [SerializeField] string _roomToLoad;
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+            SceneManager.LoadScene(_roomToLoad);
+
+    }
+
+
+
+
+
+}
