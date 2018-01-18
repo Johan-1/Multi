c84f6569c3009b334b3ea3340054b9ff3d0cc43c
Johan-1 <nilssonjohan10@gmail.com>
2018-01-18 19:48:44 +0100

    add player boundingbox in camera
    make the player be abble to move without effecting camera within a boundingbox

--- MVania/Assets/Code/CameraMovement.cs
+++ MVania/Assets/Code/CameraMovement.cs
@@ -9,52 +9,108 @@ public class CameraMovement : MonoBehaviour
     [SerializeField] Vector2 _XClampMinMax;
     [SerializeField] Vector2 _YClampMinMax;
 
+    [SerializeField] Vector2 _boundingboxXPosNeg;
+    [SerializeField] Vector2 _boundingboxYPosNeg;
+
+    // scriptable object that all cameras share
+    // can overide the shared values and use the defined ones above instead
+    [SerializeField] bool _overidePlayerBoundingbox = false;
+    [SerializeField] PlayerBoundingboxIncamera _playerBoundingbox;
+
+    Vector3 _desiredPosition;
     Transform _target;
     Vector3 _vel;
 
 	void Start () 			
 	{
+        // if we dont want to overide bounding box set the values to the ones in our scriptable objects(will be the same for all cameras in all scenes)
+        if (!_overidePlayerBoundingbox)
+        {
+            _boundingboxXPosNeg = _playerBoundingbox.boundingboxXPosNeg;
+            _boundingboxYPosNeg = _playerBoundingbox.boundingboxYPosNeg;
+        }
+        
+        // find target
         _target = FindObjectOfType<PlayerMovement>().transform;
 
-        Vector3 desiredPosition = GetDesiredPosition();
-        transform.position = new Vector3( desiredPosition.x, desiredPosition.y ,transform.position.z);
+        // set desiredpos to pos of player 
+        _desiredPosition = new Vector3(_target.position.x, _target.position.y, transform.position.z);
+
+        // clamp values and set the position
+        ClampPosition();
+        transform.position = _desiredPosition;
 
     }
 
     void LateUpdate()
     {
 
-        Vector3 desiredPosition = GetDesiredPosition();
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(desiredPosition.x,desiredPosition.y,transform.position.z), ref _vel, 0.4f);
+        GetDesiredPosition();
+        ClampPosition();
+        //transform.position = _desiredPosition;
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(_desiredPosition.x,_desiredPosition.y,transform.position.z), ref _vel, 0.1f);
     }
 
-
-    Vector3 GetDesiredPosition()
+    void GetDesiredPosition()
     {
-        Vector3 desiredPosition = _target.transform.position + new Vector3(0, 0, 0);
-        desiredPosition.x = Mathf.Clamp(desiredPosition.x, _XClampMinMax.x, _XClampMinMax.y);
-        desiredPosition.y = Mathf.Clamp(desiredPosition.y, _YClampMinMax.x, _YClampMinMax.y);
+        
+        // check if player is outside the boundingbox and move desiredpos accordingly
+        // these values will be clamped later
+        if (_target.transform.position.x > transform.position.x + (_boundingboxXPosNeg.x))
+            _desiredPosition.x = _target.transform.position.x - (_boundingboxXPosNeg.x);
+        else if (_target.transform.position.x < transform.position.x - (_boundingboxXPosNeg.y))
+            _desiredPosition.x = _target.transform.position.x + (_boundingboxXPosNeg.y);
 
-        return desiredPosition;
+        if (_target.transform.position.y > transform.position.y + (_boundingboxYPosNeg.x))
+            _desiredPosition.y = _target.transform.position.y - (_boundingboxYPosNeg.x);
+        else if (_target.transform.position.y < transform.position.y - (_boundingboxYPosNeg.y))
+            _desiredPosition.y = _target.transform.position.y + (_boundingboxYPosNeg.y);
 
+    }
+
+    void ClampPosition()
+    {
+        // adjust desiredpos to be insed clamps if neccesary
+        _desiredPosition.x = Mathf.Clamp(_desiredPosition.x, _XClampMinMax.x, _XClampMinMax.y);
+        _desiredPosition.y = Mathf.Clamp(_desiredPosition.y, _YClampMinMax.x, _YClampMinMax.y);
     }
 
     void OnDrawGizmos()
-    {
+    {        
+        Camera camera = GetComponent<Camera>();
 
         // draw the bounding box of camera clamps
-        Camera camera = GetComponent<Camera>();
-        
         float height = 2.0f * camera.orthographicSize;
         float width = height * camera.aspect;
 
         Gizmos.color = Color.red;
-        Gizmos.DrawLine(new Vector3(_XClampMinMax.y + (width * 0.5f), _YClampMinMax.y + (height * 0.5f), 0), new Vector3(_XClampMinMax.y + (width * 0.5f), _YClampMinMax.x - (height * 0.5f), 0));
-        Gizmos.DrawLine(new Vector3(_XClampMinMax.x - (width * 0.5f), _YClampMinMax.y + (height * 0.5f), 0), new Vector3(_XClampMinMax.x - (width * 0.5f), _YClampMinMax.x - (height * 0.5f), 0));
-        Gizmos.DrawLine(new Vector3(_XClampMinMax.x - (width * 0.5f), _YClampMinMax.y + (height * 0.5f), 0), new Vector3(_XClampMinMax.y + (width * 0.5f), _YClampMinMax.y + (height * 0.5f), 0));
-        Gizmos.DrawLine(new Vector3(_XClampMinMax.x - (width * 0.5f), _YClampMinMax.x - (height * 0.5f), 0), new Vector3(_XClampMinMax.y + (width * 0.5f), _YClampMinMax.x - (height * 0.5f), 0));
+        Gizmos.DrawLine(new Vector3(_XClampMinMax.y + (width * 0.5f), _YClampMinMax.y + (height * 0.5f), 0), new Vector3(_XClampMinMax.y + (width * 0.5f), _YClampMinMax.x - (height * 0.5f), 0)); // top right to bottom right
+        Gizmos.DrawLine(new Vector3(_XClampMinMax.x - (width * 0.5f), _YClampMinMax.y + (height * 0.5f), 0), new Vector3(_XClampMinMax.x - (width * 0.5f), _YClampMinMax.x - (height * 0.5f), 0)); // top left to bottom left
+        Gizmos.DrawLine(new Vector3(_XClampMinMax.x - (width * 0.5f), _YClampMinMax.y + (height * 0.5f), 0), new Vector3(_XClampMinMax.y + (width * 0.5f), _YClampMinMax.y + (height * 0.5f), 0)); // top left to top right
+        Gizmos.DrawLine(new Vector3(_XClampMinMax.x - (width * 0.5f), _YClampMinMax.x - (height * 0.5f), 0), new Vector3(_XClampMinMax.y + (width * 0.5f), _YClampMinMax.x - (height * 0.5f), 0)); // bottom left to bottom right
 
-         
+        // draw the boundingbox of player 
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(new Vector3(transform.position.x + _boundingboxXPosNeg.x, transform.position.y + _boundingboxYPosNeg.x, 0), new Vector3(transform.position.x + _boundingboxXPosNeg.x, transform.position.y - _boundingboxYPosNeg.y, 0)); // top right to bottom right
+        Gizmos.DrawLine(new Vector3(transform.position.x - _boundingboxXPosNeg.y, transform.position.y + _boundingboxYPosNeg.x, 0), new Vector3(transform.position.x - _boundingboxXPosNeg.y, transform.position.y - _boundingboxYPosNeg.y, 0)); // top left to bottom left
+        Gizmos.DrawLine(new Vector3(transform.position.x - _boundingboxXPosNeg.y, transform.position.y + _boundingboxYPosNeg.x, 0), new Vector3(transform.position.x + _boundingboxXPosNeg.x, transform.position.y + _boundingboxYPosNeg.x, 0)); // top left to top right
+        Gizmos.DrawLine(new Vector3(transform.position.x - _boundingboxXPosNeg.y, transform.position.y - _boundingboxYPosNeg.y, 0), new Vector3(transform.position.x + _boundingboxXPosNeg.x, transform.position.y - _boundingboxYPosNeg.y, 0)); // bottom left to bottom right
+
     }
+
+    // will write the playerboundingbox values on this object to our scriptableobject that all cameras share(called from custom editor)
+    public void SetPlayerBoundingboxAllCameras()
+    {
+        _playerBoundingbox.boundingboxXPosNeg = _boundingboxXPosNeg;
+        _playerBoundingbox.boundingboxYPosNeg = _boundingboxYPosNeg;
+    }
+
+    // will set the playerboundingbox values of this camera to the ones in the shared scriptable object(this happens automaticly in playmode, this is only to see it in editormode)
+    public void GetPlayerBoundingbox()
+    {
+        _boundingboxXPosNeg = _playerBoundingbox.boundingboxXPosNeg;
+        _boundingboxYPosNeg = _playerBoundingbox.boundingboxYPosNeg;
+    }
+    
 
 }
---
+++ MVania/Assets/Code/ScriptableObjectsCode/PlayerBoundingboxIncamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerCameraBoundingbox", menuName = "PlayerCamBoundingbox")]
+public class PlayerBoundingboxIncamera : ScriptableObject
+{
+
+    public Vector2 boundingboxXPosNeg;
+    public Vector2 boundingboxYPosNeg;
+
+}
